1093d76724634646c8689a0d9d298815efc52714
Ravi Peters <r.y.peters@tudelft.nl>
2022-03-01 19:56:14 +0100

    bundle all dlls on install

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-cmake -Dlaslib_DIR="C:\Users\ravi\git\geoflow-bundle\build\vcpkg_installed\x64-windows\share\lastools\LASlib" -DCMAKE_TOOLCHAIN_FILE="C:\Users\ravi\git\vcpkg\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET="x64-windows" -DVCPKG_ROOT=="C:\Users\ravi\git\vcpkg" -CMAKE_GENERATOR_PLATFORM=x64 ..
+cmake -Dlaslib_DIR="C:\Users\ravi\git\geoflow-bundle\build\vcpkg_installed\x64-windows\share\lastools\LASlib" -DCMAKE_TOOLCHAIN_FILE="C:\Users\ravi\git\vcpkg\scripts\buildsystems\vcpkg.cmake" -DVCPKG_TARGET_TRIPLET="x64-windows" -DVCPKG_ROOT=="C:\Users\ravi\git\vcpkg" -DCMAKE_GENERATOR_PLATFORM=x64 ..
 
 cmake --build . --parallel 10 --config Release
 
